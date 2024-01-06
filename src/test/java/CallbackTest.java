8b4209a92b5197b214cd6543009d2c736b9e07fa
rusLAN <puzo00712@gmail.com>
2024-01-06 13:44:54 +0300

    initial commit

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -12,13 +12,13 @@ public class CallbackTest {
     void shouldTest() throws InterruptedException {
         // Загрузить страницу
         open("http://localhost:9999");
-        //Thread.sleep(5000);
-        SelenideElement form = $("[id=root]");
+
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Роман Николаевич");
         form.$("[data-test-id=phone] input").setValue("+79618610000");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
+        $(".paragraph").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        //Thread.sleep(5000);
     }
 }
